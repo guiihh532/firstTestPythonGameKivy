b8c43113803f4ff16da34ce6114ad7aada32224c
guiihh532 <guiihh532@gmail.com>
2021-04-11 08:55:26 -0300

    Continue or other moment

--- main.py
+++ main.py
@@ -11,12 +11,31 @@ class Gerenciador(ScreenManager):
 class Player(Image):
     vel=0
     ac = -1000
-    def update(self):
-        if not collided:
+    def update(self, jump, collided):
+        #Gravity Logic
+        if self.vel > 0 and collided:
+            onGround = False
+        elif self.vel <= 0 and collided:
+            onGround = True
+        else:
+            onGround = False
+        if onGround and jump:
+            self.vel = 400
+            self.y += self.vel * 1/60
+            onGround = False
+        elif onGround:
+            self.vel = 0
+        else:
             self.vel += self.ac * 1/60
             self.y += self.vel* 1/60
-        else:
-            self.vel = 0
+
+        #Movement Logic
+        if move:                 
+            if move == 'right':
+                 self.x += 1
+            elif move == 'left':
+               self.x -= 1
+
 
 class Bloco(Image):
     px = NumericProperty(0)
@@ -25,18 +44,26 @@ class Bloco(Image):
 
 class Tela(Screen):
     blocos=[]
+    jump = False
+    move = 'right'
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Clock.schedule_once(self.desenhaTela)
-        Window.bind(on_keyboard=self.keyDown)
+        Window.bind(on_keyboard=self.keyDown, on_key_up=self.keyUp)
+
+
+    def keyUp(self, *args):
+        print(args)
 
     def keyDown(self, a,b, keycode, *args):
         if keycode == 27:
             print('jump')
-        elif keycode == 79:
-            print('direita')
+        if keycode == 79:
+            self.move = 'right'
         elif keycode == 80:
-            print('esquerda')
+            self.move = 'left'
+        else:
+            self.move = ''
 
     def collision(self):
         px = self.player.x
@@ -45,21 +72,22 @@ class Tela(Screen):
             if block.x <= px < block.x + block.width:
                 if block.y <= py < block.y + block.width:
                     
-                    
-                    return true
+                    return True
         return False
 
     def update(self, *args):
         collided = self.collision()
-        self.player.update(col)
+        self.player.update(self.jump, self.move, collided)
+        self.jump = False
 
     def desenhaTela(self, *args):
         self.player= Player()
         self.add_widget(self.player)
-        for i in range(15):
+        for i in range(40):
             self.blocos.append(Bloco(px=i))
             self.add_widget(self.blocos[-1])
-        Clock.schedule_interval(self.)
+        Clock.schedule_interval(self.update, 1/60)
+    
 
 
 class Mario(App):
